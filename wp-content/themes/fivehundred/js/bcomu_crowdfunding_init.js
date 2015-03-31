75a453b4d53e2de6a92852f00f1ff390e1c240a3
Felip Ladron <fladron@gmail.com>
2015-03-31 20:58:23 +0200

    Made the project due date translatable
    Included in plugins.js the moment.js (+ locales) library

--- wp-content/themes/fivehundred/js/bcomu_crowdfunding_init.js
+++ wp-content/themes/fivehundred/js/bcomu_crowdfunding_init.js
@@ -11,6 +11,8 @@ var config = {
   $(window).ready(function(){
     config.LANGUAGE = $('html').attr('lang');
 
+    moment.locale(config.LANGUAGE);
+
     // Crowdfunding: hide other language text divs
     $('.language-text').each(function(i){
       if ($(this).attr('lang') != icl_lang) $(this).remove();
@@ -18,6 +20,14 @@ var config = {
 
     // icons
     $('.product-post-output h2[class]').prepend('<i class="icon" />');
+
+    // translate the due date
+    var $due_date = $('.ignitiondeck .id-widget-date');
+    if ($due_date.length){
+      var real_due_date = moment($due_date.text());
+      var locale_due_date = real_due_date.format('LL');
+      $due_date.text(locale_due_date);
+    }
 
   });
 })(jQuery);
